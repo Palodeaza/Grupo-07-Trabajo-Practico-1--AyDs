15f722cc20593cc4455470710b0438919a2e55b5
tomitomitomi <tomassalig1@gmail.com>
2025-04-26 19:49:37 -0300

    UI Server

--- src/main/java/prueba/pruebaServer.java
+++ src/main/java/prueba/pruebaServer.java
@@ -1,11 +1,28 @@
 package prueba;
 
 import modelo.Server;
+import vistas.ServerView;
 
-public class pruebaServer { // esto podria estar adentro del server // termine agregandolo en server, si quieren borren esto
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import vistas.AnimatedLabel;
+
+public class pruebaServer {
     public static void main(String[] args) {
+        ServerView serverView = new ServerView();
+        serverView.setVisible(true);
+
         Server server = new Server();
-        server.iniciarServidor();        
-        System.out.println("Servidor iniciado");
+
+        serverView.getNewContactButton().addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                AnimatedLabel lbl = serverView.getNewContactButton();
+                lbl.setText("Servidor iniciado");
+                lbl.setEnabled(false);
+                lbl.removeMouseListener(this);
+                server.iniciarServidor();
+            }
+        });
     }
 }
